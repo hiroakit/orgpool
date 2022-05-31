f77c7459f778b1d0dc8b57d7cf0d0504c574c6cd
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2022-05-31 23:52:25 +0900

    Update destination path of orgpool convert feature

--- OrgPool/main.swift
+++ OrgPool/main.swift
@@ -14,32 +14,57 @@ struct OrgPool: ParsableCommand {
         commandName: "orgpool",
         abstract: "A utility for org mode files.",
         version: "0.0.1",
-        subcommands: [HtmlMeta.self, ImportWordPressCSV.self],
+        subcommands: [HtmlMeta.self, ConvertWordPressCSV.self],
         defaultSubcommand: HtmlMeta.self)
 }
 
-struct ImportWordPressCSV: ParsableCommand {
+struct ConvertWordPressCSV: ParsableCommand {
     static var configuration = CommandConfiguration(
-        commandName: "import",
-        abstract: "Import org by WordPress CSV")
+        commandName: "convert",
+        abstract: "Convert WordPress CSV to org mode files.")
     
     @Argument(
         help: "TBD.",
         completion: .file(), transform: URL.init(fileURLWithPath:))
     var inputFileUrl: URL?
+
+    @Argument(
+        help: "Destination directory",
+        completion: .directory, transform: URL.init(fileURLWithPath:))
+    var destDir: URL?
     
     mutating func run() throws {
         guard let url = inputFileUrl else {
             throw ApplicationError("Require input file url")
         }
+
+        guard let destDirUrl = destDir else {
+            throw ApplicationError("Require destination directory")
+        }
         
-        // Read org file
+        // Setup
         let expandedUrl = URL(fileURLWithPath: NSString(string: url.relativePath).expandingTildeInPath)
+        let expandedDestDirUrl = URL(fileURLWithPath: NSString(string: destDirUrl.relativePath).expandingTildeInPath)
+        
         let fileManager = FileManager.default
         guard fileManager.fileExists(atPath: expandedUrl.path) else {
             throw ApplicationError("File doesn't exsist - '\(url.relativePath)'")
         }
         
+        var isDir : ObjCBool = false
+        if !fileManager.fileExists(atPath: expandedDestDirUrl.path, isDirectory: &isDir) {
+            // Nothing (file & directory) on path
+            try fileManager.createDirectory(at: expandedDestDirUrl, withIntermediateDirectories: true)
+        } else {
+            if !isDir.boolValue {
+                // Nothing directory on path, but file exists.
+                throw ApplicationError("Found file on path. Require directory path.  - '\(destDirUrl.relativePath)'")
+            } else {
+                // Directory exists on path.
+            }
+        }
+        
+        // Read org file
         let file = FileHandle(forReadingAtPath: expandedUrl.path)
         let data = (file?.readDataToEndOfFile())!
         let dataString = String(data: data, encoding: .utf8)!
@@ -54,9 +79,9 @@ struct ImportWordPressCSV: ParsableCommand {
             print("#+data: \(value[7])")
             print("#+description: \(value[6])")
             
-            try value[5].write(to: URL(fileURLWithPath: "/Users/hiroakit/Desktop/hoge.html"), atomically: true, encoding: .utf8)
+            try value[5].write(to: URL(fileURLWithPath: "/tmp/orgpool-tmp.html"), atomically: true, encoding: .utf8)
             
-            let inputFile = FileHandle(forReadingAtPath: "/Users/hiroakit/Desktop/hoge.html")
+            let inputFile = FileHandle(forReadingAtPath: "/tmp/orgpool-tmp.html")
             let outputPipe = Pipe()
             let errorPipe = Pipe()
             let proc = Process()
@@ -94,8 +119,10 @@ struct ImportWordPressCSV: ParsableCommand {
             // Export org-mode format
             let format = DateFormatter()
             format.dateFormat = "yyyy_MM_dd-HH_mm_ss_SSS"
-            let hogename = format.string(from: Date.now)
-            try? orgString.write(toFile: "/Users/hiroakit/Desktop/file_\(hogename).org", atomically: true, encoding: .utf8)
+            let dateName = format.string(from: Date.now)
+            let fileName = "file_\(dateName).org"
+            let destPath = URL(fileURLWithPath: fileName, relativeTo: expandedDestDirUrl)
+            try? orgString.write(toFile: destPath.standardizedFileURL.path, atomically: true, encoding: .utf8)
         }
     }
 }
